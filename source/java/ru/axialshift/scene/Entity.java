14717df301d9b6175f8aff3c8109fbf0a2284307
Semion Nadezhdin <me@sg-studio.ru>
2014-08-10 13:09:51 +0400

    fixup! Ongoing work

--- source/java/ru/axialshift/scene/Entity.java
+++ source/java/ru/axialshift/scene/Entity.java
@@ -5,15 +5,13 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Entity extends TransformationProvider {
 
-	private boolean isTransformationInvalidated=true;
-	
 	public Vector3f coords;
 	public Quaternion rotation;
 	
 	
 	public void setCoords(Vector3f n){
 		coords = n;
-		isTransformationInvalidated=true;
+		markDirty();
 	}
 	public Vector3f getCoords(){
 		return coords;
--- source/java/ru/axialshift/scene/TransformationProvider.java
+++ source/java/ru/axialshift/scene/TransformationProvider.java
@@ -1,5 +1,38 @@
 package ru.axialshift.scene;
 
-public class TransformationProvider {
+import java.nio.FloatBuffer;
 
+import org.lwjgl.BufferUtils;
+import org.lwjgl.util.vector.Matrix4f;
+
+public class TransformationProvider {
+	private boolean isTransformationInvalidated=true;
+	
+	private Matrix4f model=new Matrix4f();
+	private FloatBuffer modelBuffer = BufferUtils.createFloatBuffer(4*4);
+	
+	
+	public FloatBuffer getTransformationFeed(){
+		if(isTransformationInvalidated){
+			fillMatrix();
+			fillBuffer();
+		}
+		return modelBuffer;
+	}
+
+
+	protected void fillMatrix() {
+		//Does nothing here, but extension classes fill matrix there
+	}
+
+
+	private void fillBuffer() {
+		model.store(modelBuffer);
+		modelBuffer.flip();
+	}
+	
+	protected void markDirty(){
+		isTransformationInvalidated=true;
+	}
+	
 }
